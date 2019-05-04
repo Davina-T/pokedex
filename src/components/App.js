a40d90091496505c3dad66e479a9540ac0240ee1
Davina Tan <davina.tan@connect.qut.edu.au>
2019-05-04 23:30:03 +1000

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+
+	render() {
+		return <div className="App" />;
+	}
+}
+
+export default App;
+
+// import React, { Component } from 'react';
+// import './styles/App.css';
+
+// class App extends Component {
+// 	render() {
+// 		return <div className="App">hellohello</div>;
+// 	}
+// }
+
+// export default App;
