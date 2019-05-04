95fff9caaf5525de0590a0760921dfa2379d01b8
Davina Tan <davina.tan@connect.qut.edu.au>
2019-05-05 02:09:45 +1000

    finished workshop

---
+++ src/components/PokeList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PokeCell from './PokeCell';
+import { pokeClasses } from '../pokeClasses';
+import './styles/PokeList.css';
+
+const PokeList = ({ handleOnClick }) => {
+	const cells = pokeClasses.map((pokeClass) => {
+		// prettier-ignore
+		return (
+			<PokeCell 
+				key={pokeClass.id} 
+				pokeClass={pokeClass} 
+				handleOnClick={handleOnClick} 
+			/>
+		);
+	});
+
+	return <section className="poke-list">{cells}</section>;
+};
+
+export default PokeList;
